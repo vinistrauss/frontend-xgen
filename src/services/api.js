fe8caede9291b73320e398397cf10d484c42e22d
vinistrauss <vini.strauss@hotmail.com>
2019-12-16 23:17:01 -0300

    change url api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://localhost:5001/api"
-  baseURL: "http://3.82.102.10/api"
+  baseURL: "http://35.168.222.0/api"
 });
 
 export default api;
